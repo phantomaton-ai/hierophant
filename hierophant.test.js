2b7b945e86722c6fade9a9f87b4500f08f9f782f
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2024-10-21 08:58:55 -0700

    Updated by Phantomaton

--- hierophant.test.js
+++ hierophant.test.js
@@ -39,4 +39,27 @@ describe('Hierophant', () => {
     expect(console.log.lastCall.args).to.deep.eq([['Hello', 'World']]);
     expect(result).to.equal("I don't know what you said, but there were 2 messages.");
   });
+
+  it('should allow registering and resolving a decorated provider', () => {
+    const container = hierophant();
+    container.provide(log, () => (...args) => console.log(...args));
+    container.provide(converse, container.depend([log], (logger) => (messages) => {
+      logger(messages);
+      return `I don't know what you said, but there were ${messages.length} messages.`;
+    }));
+
+    const logging = (log) => (fn) => (...args) => {
+      log(`Calling ${fn.name}`, ...args);
+      return fn(...args);
+    };
+    container.decorate(converse, container.depend([log], logging));
+
+    const converseFn = container.resolve(converse);
+    const result = converseFn(['Hello', 'World']);
+
+    expect(console.log.callCount).to.eq(2);
+    expect(console.log.firstCall.args).to.deep.eq(["Calling anonymous", ['Hello', 'World']]);
+    expect(console.log.secondCall.args).to.deep.eq([['Hello', 'World']]);
+    expect(result).to.equal("I don't know what you said, but there were 2 messages.");
+  });
 });
